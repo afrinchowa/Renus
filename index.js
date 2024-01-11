d9d600cac4a29fdc2ac295a2261ebbceaec642af
Chowa <arfinchowa524@gmail.com>
2024-01-11 20:39:35 +0600

    the project set up done

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.033buvy.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,101 +25,91 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const productCollection = client.db("productDB").collection("product");
-    const userCollection = client.db("productDB").collection("user");
+    const itemCollection = client.db("fashionDB").collection("fashion");
 
-// get product data by id
-
-    app.get("/product", async (req, res) => {
-      const cursor = productCollection.find();
+    const userCollection = client.db("fashionDB").collection("user");
+    // item crud operations
+    app.get("/items", async (req, res) => {
+      const cursor = itemCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-
-    app.get("/product/:id", async (req, res) => {
+    app.get("/items/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await productCollection.findOne(query);
+      const result = await itemCollection.findOne(query);
       res.send(result);
     });
 
-    app.post("/product", async (req, res) => {
-      const newProduct = req.body;
-      console.log(newProduct);
-      const result = await productCollection.insertOne(newProduct);
+    app.post("/items", async (req, res) => {
+      const newItem = req.body;
+      console.log(newItem);
+      const result = await itemCollection.insertOne(newItem);
       res.send(result);
     });
 
- // Add BrandNames
-app.post('/brandName', async (req, res) => {
-  try {
-    const brandName = req.body;
-    const result = await servicesBrandName.insertOne(brandName);
-    res.json({ message: 'Brand added successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+    app.put("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedItem = req.body;
+      const item = {
+        $set: {
+          name: updatedItem.name,
+          brand: updatedItem.brand,
+          type: updatedItem.type,
+          price: updatedItem.price,
+          short_description: updatedItem.short_description,
+          image: updatedItem.image,
+        },
+      };
+      const result = await itemCollection.updateOne(filter, item, options);
+      res.send(result);
+    });
 
+    app.delete("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await itemCollection.deleteOne(query);
+      res.send(result);
+    });
 
-
-
-
-
-
-
-    // user related api
+    // user crud operations/related api 
     app.get("/user", async (req, res) => {
       const cursor = userCollection.find();
       const users = await cursor.toArray();
       res.send(users);
     });
 
-
-    app.post("/user", async (req, res) => {
-      const user = req.body;
-      console.log(user);
+    app.post('/user', async(req,res) =>{
+      const user =req.body;
+      console.log(user)
       const result = await userCollection.insertOne(user);
       res.send(result);
-    });
 
-app.patch('/user', async(req, res)=>{
+    });
+app.patch('/user' , async(req,res) => {
   const user = req.body;
-  const filter = {email:user.email}
-  const updateDoc ={
-    $set:{
+  const filter ={
+    email:user.email
+  }
+  const updateDoc = {
+    $set: {
       lastLoggedAt:user.lastLoggedAt
     }
   }
-  const result =await userCollection.updateOne(filter,updateDoc)
+  const result = await userCollection.updateOne(filter,updateDoc,)
   res.send(result);
 })
 
-
-
-    app.put("/product/:id", async (req, res) => {
+    app.delete("/user/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updatedProduct = req.body;
-      const product = {
-        $set: {
-          name: updatedProduct.name,
-          brandName: updatedProduct.brandName,
-          price: updatedProduct.price,
-          type: updatedProduct.type,
-          description: updatedProduct.description,
-          photo: updatedProduct.photo,
-        },
-      };
-      const result = await productCollection.updateOne(
-        filter,
-        product,
-        options
-      );
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -136,9 +124,9 @@ app.patch('/user', async(req, res)=>{
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Brand shop server is running");
+  res.send("Fashion server is running");
 });
 
 app.listen(port, () => {
-  console.log(`Brand Server is running on port: ${port}`);
+  console.log(`Fashion server is running on port:${port}`);
 });
